01ac7207086e937e06fbb2b30843ccc425941ee9
LyxurD4 <mdoes@roc-dev.com>
2020-02-12 12:03:21 +0100

    Uitwerking van de exercise

---
+++ script.js
@@ -0,0 +1,90 @@
+function gooiButton() {
+    try {
+        bal.gooi();
+    } catch(error) {
+        document.getElementById("target").innerHTML = "Error: " + error;
+    }
+}
+
+function vangButton() {
+    try {
+        bal.vang()
+    } catch(error) {
+        document.getElementById("target").innerHTML = "Error: " + error;
+    }
+}
+
+function resetButton() {
+    try {
+        bal.reset();
+    } catch(Error) {
+        document.getElementById("target").innerHTML = "Error: " + Error;
+    }
+}
+
+var bal = {
+    canvasBal: constructBal(),
+    balPositie: "links",
+
+    gooi: function () {
+        if (this.balPositie !== "links") {
+            let number = Math.round(Math.random() * 2);
+            if (number === 0) {
+                this.referenceErrorType();
+            } else if (number === 1) {
+                this.typeErrorType();
+            } else {
+                throw Error("TypeError")
+            }
+        }
+        this.draw(300, 50);
+        this.balPositie = "midden";
+    },
+
+    vang: function () {
+        if (this.balPositie !== "midden") {
+            let number = Math.round(Math.random() * 2);
+            if (number === 0) {
+                this.referenceErrorType();
+            } else if (number === 1) {
+                this.typeErrorType();
+            } else {
+                throw Error("TypeError")
+            }
+        }
+        this.draw(500, 250);
+        this.balPositie = "rechts";
+    },
+
+    reset: function () {
+        this.draw(100, 250);
+        this.balPositie = "links";
+    },
+
+    draw: function (x, y) {
+        this.canvasBal.clearRect(0, 0, 600, 300);
+        this.canvasBal.beginPath();
+        this.canvasBal.arc(x, y, 50, 0, 2 * Math.PI);
+        this.canvasBal.closePath();
+        this.canvasBal.fill();
+    },
+
+    referenceErrorType: function () {
+        this.bestaatNiet();
+    },
+
+    typeErrorType: function () {
+        const a = 10;
+        a = 20;
+    },
+}
+
+function constructBal() {
+    let bal = document.getElementById("bal").getContext("2d");
+    bal.fillStyle = "red";
+    bal.beginPath();
+    bal.arc(100, 250, 50, 0, 2 * Math.PI);
+    bal.closePath();
+    bal.fill();
+    return bal
+}
